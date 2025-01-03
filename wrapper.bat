fdb89d344b06809ba51da40be73f8302d389c99e
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-01-03 08:26:23 +0800

    Updated to close automatically.
    Updated the wrapper to close alongside the program it wraps.

--- wrapper.bat
+++ wrapper.bat
@@ -1,5 +1,10 @@
 @echo off
-start "" "C:\Program Files (x86)\Steam\steamapps\common\Godot Engine\Godot_v4.3-stable_mono_win64.exe"
-:keep_alive
-timeout /t 600 >nul
-goto keep_alive
+
+REM Change this path if you changed your Steam app installation directory.
+set "program=C:\Program Files (x86)\Steam\steamapps\common\Godot Engine\Godot_v4.3-stable_mono_win64.exe"
+
+REM Starts the program and waits for it to close.
+start "" /wait "%program%"
+
+REM Exits the command prompt completely.
+exit /b
