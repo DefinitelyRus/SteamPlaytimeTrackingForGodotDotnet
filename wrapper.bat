0da7767753bc9abcec36ebd32f38ac5c25c8ba2a
Rus <72731965+DefinitelyRus@users.noreply.github.com>
2025-07-16 00:22:12 +0800

    Bugfixes, Updated for Godot v4.4.1

--- wrapper.bat
+++ wrapper.bat
@@ -1,10 +1,17 @@
 @echo off
 
-REM Change this path if you changed your Steam app installation directory.
-set "program=C:\Program Files (x86)\Steam\steamapps\common\Godot Engine\Godot_v4.3-stable_mono_win64.exe"
+:: Variables
+set "program=Godot_v4.4.1-stable_mono_win64.exe"
+set "godot_path=C:\Program Files (x86)\Steam\steamapps\common\Godot Engine\%program%"
 
-REM Starts the program and waits for it to close.
-start "" /wait "%program%"
+:: Start Godot
+start "" "%godot_path%" --logfile wrapper_launch.log
 
-REM Exits the command prompt completely.
-exit /b
+:: Check if still running
+:check_running
+timeout /t 60 >nul
+
+tasklist /fi "imagename eq %program%" | find /i "%program%" >nul
+
+if errorlevel 1 ( exit /b )
+else ( goto check_running )
